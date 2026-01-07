e24920153b1f047e6023f9782b9bd1f27f2db563
Burnsedia <bburnsed1995@gmail.com>
2026-01-07 13:48:16 -0500

    docs(Spawner): I need to refactor all of this

--- NPCTeamFightUnOptimized/Spawner.gd
+++ NPCTeamFightUnOptimized/Spawner.gd
@@ -6,7 +6,7 @@ extends Node3D
 @export var z_count = 1
 @export var spacing = 1
 
-
+#TODO:refactor not to be not to be O(N^3) time complexity
 func _ready():
 	var obj = load(item_path)
 	for x in range(x_count):
