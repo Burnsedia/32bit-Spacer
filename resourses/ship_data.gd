33dd5d3d11eed16d6b7a246902b6f592d6e35014
Burnsedia <bburnsed1995@gmail.com>
2026-02-21 02:43:46 -0500

    adding my shit resourses

---
+++ resourses/ship_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name ShipData
+
+@export var max_health: float = 100
+@export var max_speed: float = 100 
+@export var max_energy: int = 100
+@export var cargo_space: int = 20
+
